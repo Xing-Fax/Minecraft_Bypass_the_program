0cbe2ddbf0fc025251bec0298b83a2dcd1f1aebc
邢传真 <2895371049@qq.com>
2021-07-27 18:03:35 +0800

    修复在PID为3位的情况下无法获取到的BUG

--- Minecraft/MainWindow.xaml.cs
+++ Minecraft/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Minecraft
 {
 
@@ -32,51 +33,67 @@ namespace Minecraft
             if (e.LeftButton == MouseButtonState.Pressed) { DragMove(); }
         }
 
-        private void theout1(object source, System.Timers.ElapsedEventArgs e)
+        private void Looking_for_process(object source, System.Timers.ElapsedEventArgs e)
         {
 
-            try
+            //显示 Microsoft Store 应用及其关联的进程，按照CSV格式输出，并逐行写入到数组内
+            string[] Return_result = related_functions.CMD.RunCmd("tasklist /APPS /FO CSV").Split('\n');//获得返回信息
+
+            string result = "无结果";//存储有效结果
+
+            for (int i = 0; i < Return_result.Length; i++)//遍历全部结果，找到与Microsoft相关联的RuntimeBroker进程
             {
-                string[] str = related_functions.CMD.RunCmd("tasklist /APPS").Split('\n');//获得返回信息
-
-                string str2 = "";//存储有效结果
-
-                for (int i = 0; i < str.Length; i++)
-                {
-                    if (related_functions.Intercept.Substring(str[i], "RuntimeBroker.exe", "Microsoft.MinecraftUWP") != "")
-                    {//不等于空，说明符合条件，获取其字符串
-                        str2 = str[i];
-                    }
-                }
-
-                if (str2 != "")//在有结果的情况下
-                {
-                    t1.Stop();//暂停计时器
-                    Thread.Sleep(10000);//检测到进程后延时10秒...
-                                        //执行taskkill命令结束进程
-                    related_functions.CMD.RunCmd("taskkill /pid " + related_functions.Intercept.Substring(str2, "   ", "   ").Trim() + " /f");
-
-                    Dispatcher.Invoke(new Action(delegate
-                    {//同步线程更新状态
-                        日志.Text += "[" + DateTime.Now.ToString() + "]: 启动完成！！！\n";
-                        t1.Close();//释放计时器
-                    }));
+                if (related_functions.Intercept.Substring(Return_result[i], "RuntimeBroker.exe", "Microsoft.MinecraftUWP") != "")
+                {   //不等于空，说明符合条件，获取其字符串
+                    result = Return_result[i];
                 }
             }
-            catch(Exception ex)
+
+            if (result != "无结果")//在截取到结果后
             {
-                日志.Text = ex.ToString();
+                t1.Stop();//暂停计时器
+                Thread.Sleep(10000);//检测到进程后延时10秒...
+
+                //"RuntimeBroker.exe (runtimebroker07f4358a809ac99a64a67c1)","11680","21,608 K","Microsoft.MinecraftUWP_1.17.1004.0_x64__8wekyb3d8bbwe"
+                //获得数据后，从其中分离出PID，11680即为RuntimeBroker.exe的PID
+
+                //通过截取字符串的方法从数据中获得PID
+                string PID = related_functions.Intercept.Substring(result, "\",\"", "\",\"");
+                //使用taskkill命令结束获取到的PID进程
+                related_functions.CMD.RunCmd("taskkill /pid " + PID + " /f");
+                //同步线程更新状态
+                Dispatcher.Invoke(new Action(delegate
+                {   
+                    日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: PID = " + related_functions.Intercept.Substring(result, "\",\"", "\",\"") + "\n";
+                    日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 启动完成！！！\n";
+                    t1.Close();//释放计时器
+                }));
             }
+
         }
-
+        /// <summary>
+        /// 初始化Timer类，用于扫描进程PID，单位间隔：10000毫秒
+        /// </summary>
         System.Timers.Timer t1 = new System.Timers.Timer(10000);
+
         public MainWindow()
         {
             InitializeComponent();
 
-            日志.Text = "[" + DateTime.Now.ToString() + "]: 程序启动...\n";
+            //以下代码用作校验程序签名是否完整，调试时请将其注释
 
-            t1.Elapsed += new System.Timers.ElapsedEventHandler(theout1);//到达时间的时候执行事件
+            //if (related_functions.Fingerprint_verification.Document_verification() != true)
+            //{
+            //    MessageBox.Show("签名校验失败,程序可能被篡改,轻击确定以退出程序","警告");
+            //    Environment.Exit(0);
+            //}
+
+            //到处结束
+
+
+            日志.Text = "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 程序启动...\n";
+
+            t1.Elapsed += new System.Timers.ElapsedEventHandler(Looking_for_process) ;//到达时间的时候执行事件
             t1.AutoReset = true;//设置是执行一次（false）还是一直执行(true)
             t1.Enabled = false;//是否执行System.Timers.Timer.Elapsed事件
 
@@ -86,30 +103,31 @@ namespace Minecraft
         {
             try
             {
-                日志.Text += "[" + DateTime.Now.ToString() + "]: 读取嵌入文件字节组\n";
+                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 读取嵌入文件字节组\n";
                 byte[] byDll = Encoding.Default.GetBytes(Properties.Resources.MC_ON);//获取嵌入文件的字节数组  
-                日志.Text += "[" + DateTime.Now.ToString() + "]: 设置释放文件路径：" + Environment.GetEnvironmentVariable("TMP") + @"\MC_ON.reg" + "\n";
+                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 开始缩放内嵌注册表文件" ;
                 string strPath = Environment.GetEnvironmentVariable("TMP") + @"\MC_ON.reg";//设置释放路径
 
-                日志.Text += "[" + DateTime.Now.ToString() + "]: 写入文件流...\n";
+                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 写入文件流...\n";
                 using (FileStream fs = new FileStream(strPath, FileMode.Create))//开始写入文件流
                 {
                     fs.Write(byDll, 0, byDll.Length);
                 }
 
-                日志.Text += "[" + DateTime.Now.ToString() + "]: 写入注册表...\n";
+                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 写入注册表...\n";
                 //导入注册表
                 related_functions.Import_function.ExecuteReg(Environment.GetEnvironmentVariable("TMP") + @"\MC_ON.reg");
 
-                日志.Text += "[" + DateTime.Now.ToString() + "]: 禁用ClipSVC服务...\n";
+                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 禁用 ClipSVC 服务...\n";
                 //禁用ClipSVC服务
                 related_functions.CMD.RunCmd("net stop ClipSVC");
 
-                日志.Text += "[" + DateTime.Now.ToString() + "]: 删除临时文件...\n";
+                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 删除临时文件...\n";
                 //删除临时文件
                 File.Delete(Environment.GetEnvironmentVariable("TMP") + @"\MC_ON.reg");
 
-                日志.Text += "[" + DateTime.Now.ToString() + "]: 您现在启动您的游戏...\n";
+                //提示用户启动游戏
+                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 您现在启动您的游戏...\n";
                 //启动结束...
                 t1.Start();
             }
@@ -144,12 +162,10 @@ namespace Minecraft
             {
                 日志.Text = ex.ToString();
             }
-
         }
 
         private void 最小化_Click(object sender, RoutedEventArgs e)
         {
-            日志.Text += "[" + DateTime.Now.ToString() + "]: 最小化程序...\n";
             主窗体.WindowState = WindowState.Minimized;
         }
 
---
+++ Minecraft/related_functions/Fingerprint_verification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft.related_functions
+{
+    class Fingerprint_verification
+    {
+        /// <summary>
+        /// 检测自己身签名指纹是否匹配
+        /// 有效防止软件被病毒或恶意软件篡改
+        /// </summary>
+        /// <returns>返回true为正常状态</returns>
+        public static bool Document_verification()
+        {
+            try
+            {
+                X509Certificate cert = X509Certificate.CreateFromSignedFile(Process.GetCurrentProcess().MainModule.FileName);
+                string Fingerprint = cert.GetCertHashString();
+                if (Fingerprint == "36A888B9F2A505BF92AC6B2796C2188E639AB1D1")
+                { return true; }
+                else
+                { return false; }
+            }
+            catch { return false; }
+        }
+    }
+}
