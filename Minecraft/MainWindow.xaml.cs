6e1f376c152a521a81f3304fda4d2c06449f82db
邢传真 <2895371049@qq.com>
2021-08-05 18:59:49 +0800

    修复了只能玩30分钟的BUG

--- Minecraft/MainWindow.xaml.cs
+++ Minecraft/MainWindow.xaml.cs
@@ -33,31 +33,38 @@ namespace Minecraft
             if (e.LeftButton == MouseButtonState.Pressed) { DragMove(); }
         }
 
+        //Microsoft会在检测许可证失败后的30分钟内重新检查
+        //Microsoft检测到RuntimeBroker.exe进程消失后的20秒内重新启动该进程
+        //所以为了防止第二次检查，每隔10秒扫描并结束一次"RuntimeBroker.exe"
+        //此操作可能会造成Microsoft无响应，在一次结束"RuntimeBroker.exe"时Microsoft会恢复响应
+        private void Looking_for_RuntimeBroker(object source, System.Timers.ElapsedEventArgs e)
+        {
+            string result = related_functions.Associated_process_scan.process_scan();
+            if (result != "0")//在截取到结果后
+            {
+                //通过截取字符串的方法从数据中获得PID
+                string PID = related_functions.Intercept.Substring(result, "\",\"", "\",\"");
+                //使用taskkill命令结束获取到的PID进程
+                related_functions.CMD.RunCmd("taskkill /pid " + PID + " /f");
+            }
+        }
+
+        //Microsoft启动时会使用RuntimeBroker.exe进程来通过ClipSVC服务来检测应用许可证状态
+        //此时ClipSVC的运行状态已经被破坏掉，ClipSVC将无法重新启动
+        //由于ClipSVC无法正常启动会造成RuntimeBroker一直处于等待状态，造成程序假死，游戏加载会卡到64%
+        //只要将与Microsoft关联的RuntimeBroker.exe进程结束掉即可恢复正常状态，稍后RuntimeBroker会重新启动
         private void Looking_for_process(object source, System.Timers.ElapsedEventArgs e)
         {
-
-            //显示 Microsoft Store 应用及其关联的进程，按照CSV格式输出，并逐行写入到数组内
-            string[] Return_result = related_functions.CMD.RunCmd("tasklist /APPS /FO CSV").Split('\n');//获得返回信息
-
-            string result = "无结果";//存储有效结果
-
-            for (int i = 0; i < Return_result.Length; i++)//遍历全部结果，找到与Microsoft相关联的RuntimeBroker进程
-            {
-                if (related_functions.Intercept.Substring(Return_result[i], "RuntimeBroker.exe", "Microsoft.MinecraftUWP") != "")
-                {   //不等于空，说明符合条件，获取其字符串
-                    result = Return_result[i];
-                }
-            }
-
-            if (result != "无结果")//在截取到结果后
+            string result = related_functions.Associated_process_scan.process_scan();
+            if (result != "0")//在截取到结果后
             {
                 t1.Stop();//暂停计时器
-                Thread.Sleep(10000);//检测到进程后延时10秒...
+                Thread.Sleep(10000);//检测到进程后延时10秒...作用是等待程序加载到64%
 
                 //"RuntimeBroker.exe (runtimebroker07f4358a809ac99a64a67c1)","11680","21,608 K","Microsoft.MinecraftUWP_1.17.1004.0_x64__8wekyb3d8bbwe"
                 //获得数据后，从其中分离出PID，11680即为RuntimeBroker.exe的PID
 
-                //通过截取字符串的方法从数据中获得PID
+                //通过截取字符串的方法从数据中获得PID值
                 string PID = related_functions.Intercept.Substring(result, "\",\"", "\",\"");
                 //使用taskkill命令结束获取到的PID进程
                 related_functions.CMD.RunCmd("taskkill /pid " + PID + " /f");
@@ -66,15 +73,22 @@ namespace Minecraft
                 {   
                     日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: PID = " + related_functions.Intercept.Substring(result, "\",\"", "\",\"") + "\n";
                     日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 启动完成！！！\n";
+                    t1.Dispose();
                     t1.Close();//释放计时器
                 }));
-            }
 
+                t2.Start();//启动计时器
+            }
         }
+
         /// <summary>
-        /// 初始化Timer类，用于扫描进程PID，单位间隔：10000毫秒
+        /// 初始化Timer类，用于扫描进程PID并结束，单位间隔：15000毫秒
         /// </summary>
-        System.Timers.Timer t1 = new System.Timers.Timer(10000);
+        System.Timers.Timer t1 = new System.Timers.Timer(15000);
+        /// <summary>
+        /// 初始化Timer类，用于定时扫描进程"RuntimeBroker.exe"并结束，单位间隔：10秒
+        /// </summary>
+        System.Timers.Timer t2 = new System.Timers.Timer(10000);
 
         public MainWindow()
         {
@@ -82,86 +96,79 @@ namespace Minecraft
 
             //以下代码用作校验程序签名是否完整，调试时请将其注释
 
-            //if (related_functions.Fingerprint_verification.Document_verification() != true)
-            //{
-            //    MessageBox.Show("签名校验失败,程序可能被篡改,轻击确定以退出程序","警告");
-            //    Environment.Exit(0);
-            //}
+            if (related_functions.Fingerprint_verification.Document_verification() != true)
+            {
+                MessageBox.Show("签名校验失败,程序可能被篡改,轻击确定以退出程序", "警告");
+                Environment.Exit(0);
+            }
 
             //到处结束
-
 
             日志.Text = "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 程序启动...\n";
 
             t1.Elapsed += new System.Timers.ElapsedEventHandler(Looking_for_process) ;//到达时间的时候执行事件
-            t1.AutoReset = true;//设置是执行一次（false）还是一直执行(true)
+            t1.AutoReset = false;//设置是执行一次（false）还是一直执行(true)
             t1.Enabled = false;//是否执行System.Timers.Timer.Elapsed事件
 
+            t2.Elapsed += new System.Timers.ElapsedEventHandler(Looking_for_RuntimeBroker);//到达时间的时候执行事件
+            t2.AutoReset = true;//设置是执行一次（false）还是一直执行(true)
+            t2.Enabled = false;//是否执行System.Timers.Timer.Elapsed事件
         }
 
+        //将内嵌入注册表文件释放到用户临时文件夹后执行导入
+        //此注册表是用来破坏ClipSVC服务的正常运行，此时ClipSVC服务将无法重新启动
+        //删除释放的文件后，提示用户启动游戏
+        //并启动计时器，来后台监测与Microsoft关联的RuntimeBroker进程
         private void 启动_Click(object sender, RoutedEventArgs e)
         {
-            try
+            日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 读取嵌入文件字节组\n";
+            byte[] byDll = Encoding.Default.GetBytes(Properties.Resources.MC_ON);//获取嵌入文件的字节数组  
+            日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 开始释放内嵌注册表文件\n";
+            string strPath = Environment.GetEnvironmentVariable("TMP") + @"\MC_ON.reg";//设置释放路径
+
+            日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 写入文件流...\n";
+            using (FileStream fs = new FileStream(strPath, FileMode.Create))//开始写入文件流
             {
-                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 读取嵌入文件字节组\n";
-                byte[] byDll = Encoding.Default.GetBytes(Properties.Resources.MC_ON);//获取嵌入文件的字节数组  
-                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 开始缩放内嵌注册表文件" ;
-                string strPath = Environment.GetEnvironmentVariable("TMP") + @"\MC_ON.reg";//设置释放路径
-
-                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 写入文件流...\n";
-                using (FileStream fs = new FileStream(strPath, FileMode.Create))//开始写入文件流
-                {
-                    fs.Write(byDll, 0, byDll.Length);
-                }
-
-                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 写入注册表...\n";
-                //导入注册表
-                related_functions.Import_function.ExecuteReg(Environment.GetEnvironmentVariable("TMP") + @"\MC_ON.reg");
-
-                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 禁用 ClipSVC 服务...\n";
-                //禁用ClipSVC服务
-                related_functions.CMD.RunCmd("net stop ClipSVC");
-
-                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 删除临时文件...\n";
-                //删除临时文件
-                File.Delete(Environment.GetEnvironmentVariable("TMP") + @"\MC_ON.reg");
-
-                //提示用户启动游戏
-                日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 您现在启动您的游戏...\n";
-                //启动结束...
-                t1.Start();
-            }
-            catch(Exception ex)
-            {
-                日志.Text = ex.ToString();
+                fs.Write(byDll, 0, byDll.Length);
             }
 
+            日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 写入注册表...\n";
+            //导入注册表
+            related_functions.Import_function.ExecuteReg(Environment.GetEnvironmentVariable("TMP") + @"\MC_ON.reg");
+
+            日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 禁用 ClipSVC 服务...\n";
+            //禁用ClipSVC服务
+            related_functions.CMD.RunCmd("net stop ClipSVC");
+
+            日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 删除临时文件...\n";
+            //删除临时文件
+            File.Delete(Environment.GetEnvironmentVariable("TMP") + @"\MC_ON.reg");
+
+            //提示用户启动游戏
+            日志.Text += "[" + DateTime.Now.ToLongTimeString().ToString() + "]: 您现在启动您的游戏...\n";
+            //启动结束...
+            t1.Start();
         }
 
+        //导入原注册表并恢复ClipSVC服务的正常运行
         private void 关闭_Click(object sender, RoutedEventArgs e)
         {
-            try
+
+            主窗体.Visibility = Visibility.Collapsed;//隐藏主窗体
+            byte[] byDll = Encoding.Default.GetBytes(Properties.Resources.MC_OFF);//获取嵌入文件的字节数组  
+            string strPath = Environment.GetEnvironmentVariable("TMP") + @"\MC_OFF.reg";//设置释放路径
+            using (FileStream fs = new FileStream(strPath, FileMode.Create))//开始写入文件流
             {
-                主窗体.Visibility = Visibility.Collapsed;//隐藏主窗体
-                byte[] byDll = Encoding.Default.GetBytes(Properties.Resources.MC_OFF);//获取嵌入文件的字节数组  
-                string strPath = Environment.GetEnvironmentVariable("TMP") + @"\MC_OFF.reg";//设置释放路径
-                using (FileStream fs = new FileStream(strPath, FileMode.Create))//开始写入文件流
-                {
-                    fs.Write(byDll, 0, byDll.Length);
-                }
-                //恢复注册表
-                related_functions.Import_function.ExecuteReg(Environment.GetEnvironmentVariable("TMP") + @"\MC_OFF.reg");
-                //恢复ClipSVC服务
-                related_functions.CMD.RunCmd("net start ClipSVC");
-                //删除临时文件
-                File.Delete(Environment.GetEnvironmentVariable("TMP") + @"\MC_OFF.reg");
-                //关闭进程
-                Environment.Exit(0);
+                fs.Write(byDll, 0, byDll.Length);
             }
-            catch(Exception ex)
-            {
-                日志.Text = ex.ToString();
-            }
+            //恢复注册表
+            related_functions.Import_function.ExecuteReg(Environment.GetEnvironmentVariable("TMP") + @"\MC_OFF.reg");
+            //恢复ClipSVC服务
+            related_functions.CMD.RunCmd("net start ClipSVC");
+            //删除临时文件
+            File.Delete(Environment.GetEnvironmentVariable("TMP") + @"\MC_OFF.reg");
+            //关闭进程
+            Environment.Exit(0);
         }
 
         private void 最小化_Click(object sender, RoutedEventArgs e)
